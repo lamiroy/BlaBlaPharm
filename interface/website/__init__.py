0d5929bbf0a36cebecec7b3dd2d5b45b7ca214f3
EliottPommier <98904225+EliottPommier@users.noreply.github.com>
2022-04-18 19:12:19 +0200

    Ajout adresses front

---
+++ interface/website/__init__.py
@@ -0,0 +1,25 @@
+from gzip import READ
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+import csv
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
+    from .models import User, Note, VI, Expressions, Delivery, Invoicing, Ordering, Remuneration, Sales, WorkEvents
+
+    return app
